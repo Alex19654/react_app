35227f9adf8e985353dcd6e37b0bb30746d5618d
Alex19654 <podgorodeczkij19@gmail.com>
2021-03-08 15:56:30 +0200

    First commit of the project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js'
+import Footer from './Footer';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <Header />
+
+        <Footer />
+      </div>
+    </div>
+
+  );
+}
+
+
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+const Header = () => {
+  return (
+    <div>
+      Simple HTML
+      Simple HTML
+      Simple HTML
+      Simple HTML
+      Simple HTML
+      Simple HTML
+
+    </div>
+
+  );
+}
+
+
+
+export default Header;
